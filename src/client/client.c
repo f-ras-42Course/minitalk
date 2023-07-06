c2a9e092ddb9d1c1760ea8da59a63d2c1e22c35c
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-07-06 23:26:08 +0200

    improved validations

--- src/client/client.c
+++ src/client/client.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/05 20:40:12 by fras          #+#    #+#                 */
-/*   Updated: 2023/07/06 23:10:37 by fras          ########   odam.nl         */
+/*   Updated: 2023/07/06 23:18:51 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,5 @@ int	main(int argc, char **argv)
 {
 	if(invalid_userinput(argc, argv))
 		return(EXIT_FAILURE);
-	ft_printf("MAX PID: %d", PID_MAX_LIMIT);
 	return(EXIT_SUCCESS);
 }
--- src/client/validations.c
+++ src/client/validations.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/06 21:12:50 by fras          #+#    #+#                 */
-/*   Updated: 2023/07/06 23:10:42 by fras          ########   odam.nl         */
+/*   Updated: 2023/07/06 23:25:49 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 bool	invalid_userinput(int argc, char **argv)
 {
-	if (argc != 2)
+	if (argc != 3)
 		return(user_mistake(unexpected_argument_qty));
 	if (!string_is_digit_only(argv[1]))
 		return(user_mistake(invalid_PID_format));
@@ -26,10 +26,12 @@ bool	invalid_userinput(int argc, char **argv)
 bool	user_mistake(t_user_error code)
 {
 	if (code == unexpected_argument_qty)
-		write(STDERR_FILENO,"ERROR: expected one argument.\n", 31);
+		write(STDERR_FILENO,"ERROR: expected two arguments.\n", 31);
 	if (code == invalid_PID_format)
-		write(STDERR_FILENO,"ERROR: only digits in PID format allowed.\n", 43);
+		write(STDERR_FILENO,"ERROR: expects only digits in PID format.\n", 43);
 	if (code == pid_max_exceeded)
 		write(STDERR_FILENO, "ERROR: MAX_PID 4194304 exceeded.\n", 34);
+	ft_printf("\nStart program with the following arguments: ");
+	ft_printf("./client [server PID] [\" string to send \"]\n");
 	return (true);
 }
